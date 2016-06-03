66785ccc0f25bd9c8eafda5c20f810ce515b822f
ania <ania.polec@gmail.com>
2016-06-04 01:48:05 +0200

    Fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,14 @@ class UsersController < ApplicationController
     if @user = User.find_by(email: params[:email])
       render json: @user
     else
-      @user = User.create(params[:email])
+      @user = User.create(user_params)
       render json: @user
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email)
   end
 end
