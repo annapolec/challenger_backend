f24fe833048deb08dbe73c74f8938c3f691b14d1
marta <polec.marta@gmail.com>
2016-06-05 22:38:33 +0200

    Fixes in sync method

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -2,9 +2,9 @@ class SyncController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @battles = @user.all_battles
-    @battle_members = BattleMember.where(battle_id: @user.all_battles.pluck(:id))
+    @battle_members = BattleMember.where(battle_id: @user.all_battles.map { |battle| battle.id })
     @challenges = @user.all_challenges
-    @challenge_members = ChallengeMembers.where(challenge_id: @user.all_challenges.pluck(:id))
+    @challenge_members = ChallengeMember.where(challenge_id: @user.all_challenges.map { |challenge| challenge.id })
     @users = User.all
     
     render json: { battles: @battles, 
